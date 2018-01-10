34ba3f0c1023ce93b57c25ddcc0357c4373ce3f0
adilsaidagali <adilsaidagali@gmail.com>
2018-01-10 09:58:14 +0600

    Lab1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string s in args)
+            {
+                bool t = true;
+                for (int i = 2; i < int.Parse(s); i++)
+                    if (int.Parse(s) % i == 0)
+                        t = false;
+                if (t == true)
+                    Console.WriteLine(int.Parse(s));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Student
+{
+    public string name;
+    public int age;
+    public float gpa;
+    public Student(string n, int a, float g)
+    {
+        name = n;
+        age = a;
+        gpa = g;
+    }
+    public override string ToString()
+    {
+        return name + " " + age + " " + gpa;
+    }
+}
