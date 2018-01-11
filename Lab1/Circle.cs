fac8a2832247b481d08b2d38b920baf64961cc7e
adilsaidagali <adilsaidagali@gmail.com>
2018-01-11 20:30:25 +0600

    Lab1

---
+++ Lab1/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Circle
+{
+    public int Radius, Area;
+	public Circle(int r)
+	{
+        Radius = r;
+	}
+    public Circle()
+    {
+        Radius = 0;
+    }
+    public static float FindArea(int r)
+    {
+        return Math.PI * r * r;
+    }
+    public override string ToString()
+    {
+        return Radius;
+    }
+}
---
+++ Lab1/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Rectangle
+{
+    public int Width, Length;
+    public Rectangle(int w, int l)
+    {
+        Width = w;
+        Length = l;
+    }
+    public static float FindArea(int w, int l)
+    {
+        return w * l;
+    }
+    public static int FindPerimetr(int w, int l)
+    {
+        return w + l + w + l;
+    }
+    public Rectangle()
+    {
+        Width = 0;
+        Length = 0;
+    }
+    public override string ToString()
+    {
+        return Width + " " + Length;
+    }
+}
