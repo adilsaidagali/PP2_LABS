a141bc51905f9c0764b7d6b47090cb4428629a19
adilsaidagali <adilsaidagali@gmail.com>
2018-01-13 01:34:54 +0600

    Lab1

--- Lab1/Circle.cs
+++ Lab1/Circle.cs
@@ -2,18 +2,22 @@
 
 public class Circle
 {
-    public int Radius, Area;
-	public Circle(int r)
-	{
+    public float Radius;
+    public Circle(float r)
+    {
         Radius = r;
-	}
+    }
     public Circle()
     {
         Radius = 0;
     }
-    public static float FindArea(int r)
+    public static float FindArea(float r)
     {
         return Math.PI * r * r;
+    }
+    public static float FindLength(float r)
+    {
+        return Math.PI * 2 * r;
     }
     public override string ToString()
     {
