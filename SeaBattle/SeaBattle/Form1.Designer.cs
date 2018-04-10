d908eadfd0e26140ae31272616867d5aae9e43ae
adilsaidagali <adilsaidagali@gmail.com>
2018-04-10 12:56:47 +0600

    SeaBattle

---
+++ SeaBattle/SeaBattle/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace SeaBattle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(338, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Horizontal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Image = global::SeaBattle.Properties.Resources._1boat;
+            this.button5.Location = new System.Drawing.Point(338, 236);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(55, 29);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Image = global::SeaBattle.Properties.Resources._2boat;
+            this.button4.Location = new System.Drawing.Point(338, 188);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 29);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Image = global::SeaBattle.Properties.Resources._3boat;
+            this.button3.Location = new System.Drawing.Point(338, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(55, 29);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::SeaBattle.Properties.Resources._4boat;
+            this.button2.Location = new System.Drawing.Point(338, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 29);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pictureBox2.Location = new System.Drawing.Point(399, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 320);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 320);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(358, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(358, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(358, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 395);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SeaBattle/SeaBattle/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeaBattle
+{
+    public partial class Form1 : Form
+    {
+        Pen pen = new Pen(Color.Black, 2);
+        bool clicked;
+        bool[][] map;
+        Bitmap bmp, bmp1;
+        Graphics g, g1;
+        Point cur, prev;
+        public enum Tool
+        {
+            boat1,
+            boat2,
+            boat3,
+            boat4
+        };
+        Tool tool;
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            g = Graphics.FromImage(bmp);
+            tool = Tool.boat4;
+            pictureBox1.Image = bmp;
+            bmp1 = new Bitmap(pictureBox2.Size.Width, pictureBox2.Size.Height);
+            g1 = Graphics.FromImage(bmp1);
+            pictureBox2.Image = bmp1;
+            for (int i = 0; i < 11; i++)
+                g.DrawLine(pen, 32 * i, 0, 32 * i, 320);
+            for (int i = 0; i < 11; i++)
+                g.DrawLine(pen, 0, 32 * i, 320, 32 * i);
+            pictureBox1.Refresh();
+            for (int i = 0; i < 11; i++)
+                g1.DrawLine(pen, 32 * i, 0, 32 * i, 320);
+            for (int i = 0; i < 11; i++)
+                g1.DrawLine(pen, 0, 32 * i, 320, 32 * i);
+            pictureBox2.Image = bmp1;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (button1.Text == "Horizontal" && clicked)
+            {
+                if (tool == Tool.boat4)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.X > 192)
+                        x = 192;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Red), x, y, 128, 32);
+                }
+                if (tool == Tool.boat3)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.X > 224)
+                        x = 224;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Red), x, y, 96, 32);
+                }
+                if (tool == Tool.boat2)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.X > 256)
+                        x = 256;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Red), x, y, 64, 32);
+                }
+                if (tool == Tool.boat1)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Red), x, y, 32, 32);
+                }
+            }
+            if (button1.Text == "Vertical" && clicked)
+            {
+                if (tool == Tool.boat4)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.Y > 192)
+                        y = 192;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Red), x, y, 32, 128);
+                }
+                if (tool == Tool.boat3)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.Y > 224)
+                        y = 224;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Red), x, y, 32, 96);
+                }
+                if (tool == Tool.boat2)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.Y > 256)
+                        y = 256;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Red), x, y, 32, 64);
+                }
+                if (tool == Tool.boat1)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Red), x, y, 32, 32);
+                }
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Horizontal")
+                button1.Text = "Vertical";
+            else
+                button1.Text = "Horizontal";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tool = Tool.boat4;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tool = Tool.boat3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            tool = Tool.boat2;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tool = Tool.boat1;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            clicked = true;
+            pen = new Pen(Color.DarkRed, 2);
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            cur = e.Location;
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (button1.Text == "Horizontal" && clicked)
+            {
+                if (tool == Tool.boat4)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.X > 192)
+                        x = 192;
+                    g.FillRectangle(new SolidBrush(Color.Red), x, y, 128, 32);
+                }
+                if (tool == Tool.boat3)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.X > 224)
+                        x = 224;
+                    g.FillRectangle(new SolidBrush(Color.Red), x, y, 96, 32);
+                }
+                if (tool == Tool.boat2)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.X > 256)
+                        x = 256;
+                    g.FillRectangle(new SolidBrush(Color.Red), x, y, 64, 32);
+                }
+                if (tool == Tool.boat1)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    g.FillRectangle(new SolidBrush(Color.Red), x, y, 32, 32);
+                }
+            }
+            if (button1.Text == "Vertical" && clicked)
+            {
+                if (tool == Tool.boat4)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.Y > 192)
+                        y = 192;
+                    g.FillRectangle(new SolidBrush(Color.Red), x, y, 32, 128);
+                }
+                if (tool == Tool.boat3)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.Y > 224)
+                        y = 224;
+                    g.FillRectangle(new SolidBrush(Color.Red), x, y, 32, 96);
+                }
+                if (tool == Tool.boat2)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    if (cur.Y > 256)
+                        y = 256;
+                    g.FillRectangle(new SolidBrush(Color.Red), x, y, 32, 64);
+                }
+                if (tool == Tool.boat1)
+                {
+                    int x, y;
+                    x = cur.X / 32;
+                    x *= 32;
+                    y = cur.Y / 32;
+                    y *= 32;
+                    g.FillRectangle(new SolidBrush(Color.Red), x, y, 32, 32);
+                }
+            }
+            clicked = false;
+        }
+    }
+}
